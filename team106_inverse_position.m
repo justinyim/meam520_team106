c1fe9ece0ddc54d18ca1114048602fb4397da7bd
Justin <justin@jyim-E6510.(none)>
2014-10-19 12:35:28 -0400

    Commented team106_inverse_position.m

--- team106_inverse_position.m
+++ team106_inverse_position.m
@@ -1,7 +1,9 @@
 function [thetas] = team106_inverse_position(x,y,z)
 
+%Initialize output
 thetas = nan(3,4);
 
+%Robot parameters
 a = 13;
 b = 2.5;
 c = 8;
@@ -9,18 +11,19 @@ d = 2.5;
 e = 8;
 %f = 2.5;
 
+%Theta 1
 phi1 = atan2(y,x);
-alpha1 = atan2(b+d,sqrt(x^2+y^2-(b+d)^2));
+r = sqrt(x^2 + y^2 - (b+d)^2);
+alpha1 = atan2(b+d,r);
+
 thetas(1,1) = phi1-alpha1; %left, down
 thetas(1,2) = phi1+alpha1+pi; %right, down
 thetas(1,3) = thetas(1,1); %left, up
 thetas(1,4) = thetas(1,2); %right, up
 
-r = sqrt(x^2 + y^2 - (b+d)^2);
-
+%Theta 2 and Theta 3
 rho = sqrt(r^2 + (z-a)^2);
-gamma2 = acos((c^2 + e^2 - rho^2)/(2*c*e));
-
+gamma3 = acos((c^2 + e^2 - rho^2)/(2*c*e));
 alpha2 = acos((rho^2 + c^2 - e^2)/(2*rho*c));
 epsilon2 = atan2(z-a,r);
 
@@ -29,8 +32,8 @@ thetas(2,2) = pi - thetas(2,1); %right, down
 thetas(2,3) = epsilon2 + alpha2; %left, up
 thetas(2,4) = pi - thetas(2,3); %right, up
 
-thetas(3,1) = pi/2 - gamma2; %left, down
-thetas(3,2) = pi/2 + gamma2; %right, down
+thetas(3,1) = pi/2 - gamma3; %left, down
+thetas(3,2) = pi/2 + gamma3; %right, down
 thetas(3,3) = thetas(3,2); %left, up
 thetas(3,4) = thetas(3,1); %right, up
 
